677fcdd75c07c7cb8df6217fe9ac01f5436dad4c
Shohin <shohin_6707@mail.ru>
2020-01-10 13:53:39 +0500

    fixes #6

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@
 const regular = 0.01;
 const increased = 0.05;
 const special = 0.3;
+const cashbackLimit = 3000;
 
 const flash = {
     price: 250,
@@ -18,10 +19,11 @@ const SSD = {
     price: 1000,
     category: special
 };
-let cash = (flash.price*flash.category)+(HDD.price*HDD.category)+(SSD.price*SSD.category)
 
-if (cash > 3000){
-    cash = 3000;
+let cash = (flash.price*flash.category)+(HDD.price*HDD.category)+(SSD.price*SSD.category);
+
+if (cash > cashbackLimit){
+    cash = cashbackLimit;
 };
 
 console.log (`Кэшбэк равен: `+cash);
